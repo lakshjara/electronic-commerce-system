48a796cf8702e4bf209619137d4bb59935916fee
lakshjara <a01713328@tec.mx>
2024-11-08 20:35:10 -0600

    Create Cart.cpp

---
+++ Cart.cpp
@@ -0,0 +1,13 @@
+#include "Cart.h"
+
+void Cart::addItem(Product product, int quantity) {
+    // Método vacío
+}
+
+void Cart::removeItem(Product product) {
+    // Método vacío
+}
+
+double Cart::getTotal() {
+    return 0.0;
+}
