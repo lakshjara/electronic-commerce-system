f310503a6c02ae5118c59a9ac886c806d55299ae
lakshjara <a01713328@tec.mx>
2024-11-20 18:12:01 -0600

    Delete Payment.cpp

--- Payment.cpp
+++
@@ -1,8 +0,0 @@
-#include "Payment.h"
-
-Payment::Payment(int paymentId, double amount, std::string method)
-    : paymentId(paymentId), amount(amount), method(method) {}
-
-bool Payment::processPayment() {
-    return true;
-}
